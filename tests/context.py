8ce32f5b01bd62100fcfc045a7a7950d7e2c27a7
Niccolò Marcon <marcon.niccolo@gmail.com>
2018-11-23 22:42:41 +0100

    Add tests for Production class

---
+++ tests/context.py
@@ -0,0 +1,6 @@
+# Hack from https://docs.python-guide.org/writing/structure/
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import lfc
---
+++ tests/test_Production.py
@@ -0,0 +1,32 @@
+from context import lfc
+
+
+class TestProduction():
+    def test_base_constructor(self):
+        driver = ['S']
+        body = ['a', 'S', 'b']
+        production = lfc.Production(driver=driver, body=body)
+        assert driver == production.driver
+        assert body == production.body
+        assert driver is not production.driver
+        assert body is not production.body
+
+    def test_from_text(self):
+        text = 'S->aSb'
+        production = lfc.Production.from_text(text=text)
+        assert production.driver == ['S']
+        assert production.body == ['a', 'S', 'b']
+
+        text = 'Ab->'
+        production = lfc.Production.from_text(text=text)
+        assert production.driver == ['A', 'b']
+        assert not production.body
+
+    def test_str(self):
+        text = 'S->aSb'
+        production = lfc.Production.from_text(text=text)
+        assert text == production.__str__()
+
+        text = 'Ab->'
+        production = lfc.Production.from_text(text=text)
+        assert text + '𝜀' == production.__str__()
